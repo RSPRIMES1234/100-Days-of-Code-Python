a6335ae23285f9b57f60ad9d3523c6b4d7acd2fc
Amritesh Chandra <103377226+RSPRIMES1234@users.noreply.github.com>
2024-07-31 19:21:50 +0530

    Add files via upload

---
+++ 34th/data.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+parameters={"amount":10,
+            "type":"boolean",
+            "category":18}
+
+url="https://opentdb.com/api.php"
+
+response=requests.get(url=url,params=parameters)
+response.raise_for_status()
+
+
+question_data = response.json()["results"]
